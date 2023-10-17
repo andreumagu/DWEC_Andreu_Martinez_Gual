a20fb31176e80c55a353262a509f936d57d49e97
andreumagu <andreumagu@gmail.com>
2023-10-17 08:27:19 +0200

    OK

---
+++ Prueba/script.js
@@ -0,0 +1,4 @@
+let edat = 23, nueva_edad, increment = 4;
+const nom = "Rosa García";
+console.log(typeof increment === "number");
+nueva_edad = edat + increment; console.log(nom+ " després de " + increment + " anys tindrà "+nueva_edad);
